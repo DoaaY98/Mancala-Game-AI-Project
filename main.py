8356a78ab7b60c8f9b7c6fdbe14a24c2c094ca33
DoaaY98 <doaayramadan@gmail.com>
2021-06-07 04:12:00 +0200

    Added multiple helper_functions
    Added collect_remainder_Stones
    edited decide_winner and is_game_over
    added play_move fn with its two players helping fns

---
+++ main.py
@@ -0,0 +1,21 @@
+import helper_functions as fn
+
+board=fn.initialize_board()
+board[0]=[25,0,0,0,0,0,0,0]
+board[1]=[0,2,2,2,2,2,2,4]
+
+#print(play_move(board,0,0))
+
+
+##last_location , next_player=play_move(board,0,0)
+
+while(not fn.is_game_over(board)):
+    #el3aaab
+
+    
+    #print(play_move(board,0,0))
+
+    pass
+
+if(fn.is_game_over(board)):
+    print(fn.decide_winner(board))
