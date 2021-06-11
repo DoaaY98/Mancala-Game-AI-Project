c5b62ecdbf843083580e10c58e0ad6c7232e8baf
DoaaY98 <doaayramadan@gmail.com>
2021-06-12 00:32:39 +0200

    added save and load feature
    if you want to save the game press quit during your turn..
    
    if you want to load your last game press Y when asked

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import helper_functions as fn
 import time
 global start_time
+import numpy as np
+import save_load as sv
 #import minimax as minimax
-board = fn.initialize_board()
+#board = fn.initialize_board()
 #board[1] = [15,0,6,0,0,0,14,0]
 #board[0] = [0,0,0,0,0,0,0,12]
 #fn.print_board(board)
 if __name__ == "__main__":
+
+    board_loaded=sv.load_game()
+    saved=0
 
     print("-------------------**************----------------------")
     print("||               Welcome to Mancala Game             ||")
@@ -26,23 +31,77 @@ if __name__ == "__main__":
     print("-------------------**************----------------------")
     print("-------------------**************----------------------")
     print("||                 LET'S START                       ||")
-
-    
-    starting = input("Do You Like to Start? \n if Yes Press Y  \n if You want the COMPUTER to start Press any other key.... \n")
     player='0'
-    if(starting=='Y' or starting== 'y'):
-        player ='1'
-    else :
-        player='0'
+    mode ='0'
+    board=np.array([])
+    if(board_loaded.size==0):
+        board = fn.initialize_board()
+        print("-------------------**************----------------------")
+        print("-------------------**************----------------------")
+        print("||                 NEW GAME STARTING....             ||")
+        print("-------------------**************----------------------")
+        print("-------------------**************----------------------")
+        starting = input("Do You Like to Start? \n if Yes Press Y  \n if You want the COMPUTER to start Press any other key.... \n")
+        
+        if(starting=='Y' or starting== 'y'):
+            player ='1'
+        else :
+            player='0'
 
     
-    stealing_mode= input("Would You like to enter STEALING MODE? \n if Yes Press Y  \n if NO then press any other key.... \n")
+        stealing_mode= input("Would You like to enter STEALING MODE? \n if Yes Press Y  \n if NO then press any other key.... \n")
 
-    mode ='0'
-    if(stealing_mode=='Y' or stealing_mode== 'y'):
-        mode='0'
+        if(stealing_mode=='Y' or stealing_mode== 'y'):
+            mode='0'
+        else:
+            mode='1'
+
     else:
-        mode='1'
+        starting = input("Would you like to LOAD Last game? \n if Yes Press 'Y'  \n if You want to start a NEW GAME  Press any other key.... \n")
+
+        if(starting=='Y' or starting== 'y'):
+            #loading last game
+            board=board_loaded
+            player ='1'
+            print("-------------------**************----------------------")
+            print("-------------------**************----------------------")
+            print("||                LOADING LAST GAME....             ||")
+            print("-------------------**************----------------------")
+            print("-------------------**************----------------------")
+            stealing_mode= input("Would You like to enter STEALING MODE? \n if Yes Press Y  \n if NO then press any other key.... \n")
+
+
+            if(stealing_mode=='Y' or stealing_mode== 'y'):
+                mode='0'
+            else:
+                mode='1'
+
+
+        else:
+
+            board = fn.initialize_board()
+            print("-------------------**************----------------------")
+            print("-------------------**************----------------------")
+            print("||                 NEW GAME STARTING....             ||")
+            print("-------------------**************----------------------")
+            print("-------------------**************----------------------")
+            starting = input("Do You Like to Start? \n if Yes Press Y  \n if You want the COMPUTER to start Press any other key.... \n")
+        
+            if(starting=='Y' or starting== 'y'):
+                player ='1'
+            else :
+                player='0'
+
+    
+            stealing_mode= input("Would You like to enter STEALING MODE? \n if Yes Press Y  \n if NO then press any other key.... \n")
+
+            if(stealing_mode=='Y' or stealing_mode== 'y'):
+                mode='0'
+            else:
+                mode='1'
+
+
+
 
 
     print("Game Started.................")
@@ -51,6 +110,11 @@ if __name__ == "__main__":
     else:
         print("STEALING MODE IS OFF ...")
     
+    print("-------------------**************-------------------------------------------------------")
+    print("-------------------**************-------------------------------------------------------")
+    print("|| if you want to save and exit at any time write 'quit' when it's your turn          ||")
+    print("-------------------**************-------------------------------------------------------")
+    print("-------------------**************-------------------------------------------------------")
     fn.print_board(board)
     print("\n")
     next_player = 0
@@ -73,23 +137,48 @@ if __name__ == "__main__":
             print("Elapsed time: " + str(elapsed_time))
         elif player == "1":  #  player to start
             player = 1
-            move = int(input("YOUR TURN \n Choose move between 1 --> 6 :\n"))
+            your_turn = (input("YOUR TURN \n Choose move between 1 --> 6 :\n"))
+            
             valid_moves = fn.get_valid_moves(board, int(player))
-            if move in valid_moves:
-                last_location, next_player = fn.play_move(board, move, int(player))
-                if mode=='0':
-                    fn.stealing_mode(board, move, int(player),last_location)
+            
+            if your_turn=='quit' or your_turn=='QUIT' or your_turn=='Quit':
+
+
+                print("-------------------**************----------------------")
+                print("-------------------**************----------------------")
+                print("||                SAVING GAME             ||")
+                print("-------------------**************----------------------")
+                print("-------------------**************----------------------")
+                sv.save_game(board)
+                print("-------------------**************----------------------")
+                print("-------------------**************----------------------")
+                print("||                GOODBYE                  ||")
+                print("-------------------**************----------------------")
+                print("-------------------**************----------------------")
+                saved=1
+                break;
             else:
-                print("inValid Move xxxx")
-                print("Please choose a valid move ........")
-                #print("-----------------------------------------")
-                print("\n")
-                next_player = str(player)
+                move=int(your_turn)
+                if move in valid_moves:
+                    last_location, next_player = fn.play_move(board, move, int(player))
+                    if mode=='0':
+                        fn.stealing_mode(board, move, int(player),last_location)
+                else:
+                    print("inValid Move xxxx")
+                    print("Please choose a valid move ........")
+                    #print("-----------------------------------------")
+                    print("\n")
+                    next_player = str(player)
         fn.print_board(board)
         print("\n")
         player = str(next_player)
-if fn.is_game_over(board):
-        print(fn.decide_winner(board))
-        fn.print_board(board)
+
+
+
+    if(saved==1):
+        save=0
+    elif fn.is_game_over(board):
+            print(fn.decide_winner(board))
+            fn.print_board(board)
 
 
